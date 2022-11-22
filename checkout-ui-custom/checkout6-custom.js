5ea3c58f7cfe9f6592f0709d91187515ecfac5b1
Roberto Ramirez <robert.624@hotmail.com>
2022-11-22 11:08:26 -0500

    added orderForm title

--- checkout-ui-custom/checkout6-custom.js
+++ checkout-ui-custom/checkout6-custom.js
@@ -11,8 +11,6 @@ const shoppingCart = {
         headerLink.text('')
 
         headerLink.append('<img class="header-logo" src="/arquivos/logo.svg"/>')
-
-        console.log("GTM header anchor -> ", header)
     },
   },
 
@@ -20,35 +18,61 @@ const shoppingCart = {
     init: function(){
         $('#cart-choose-products').prepend('<img src="/arquivos/left-arrow-white.svg" />')
 
+        const toOrderFormBtn = $('#cart-to-orderform')
+
+        toOrderFormBtn.prepend('<img class="payments-checkout" src="/arquivos/header-payments.svg"/>')
+        toOrderFormBtn.append('<img class="proceed-arrow" src="/arquivos/right-arrow-white.svg" />')
+
         const mainCart = $('.container-cart')
 
         const reassuranceBar = `
+        <div class="outer-reassurance">
         <div class="reassurance-bar">
             <div class="reassurance-item">
-                <img reassurance-item-image src="/arquivos/reassurance-5.svg" />
+                <img class="reassurance-item-image five-icon" src="/arquivos/reassurance-5.svg" />
                 <div class="reassurance-item-text">
                     <p class="reassurance-title">5 year guarantee</p>
                     <p class="reassurance-subtitle">Peace of mind at no extra cost</p>
                 </div>
             </div>
             <div class="reassurance-item">
-                <img reassurance-item-image src="/arquivos/segtigo-logo.svg" />
+                <img class="reassurance-item-image segtigo-icon" src="/arquivos/segtigo-logo.svg" />
                 <div class="reassurance-item-text">
                     <p class="reassurance-title">Fast, Easy & Safe</p>
                     <p class="reassurance-subtitle">100% Secure Checkout</p>
                 </div>
             </div>
             <div class="reassurance-item">
-                <img reassurance-item-image src="/arquivos/reassurance-truck.svg" />
+                <img class="reassurance-item-image truck-icon" src="/arquivos/reassurance-truck.svg" />
                 <div class="reassurance-item-text">
                     <p class="reassurance-title">Free UK Delivery</p>
                     <p class="reassurance-subtitle">On all orders over £199</p>
                 </div>
             </div>
         </div>
+        </div>
         `
 
-        mainCart.prepend(reassuranceBar)
+        const orderFormHeader = `
+          <div class="order-form-header">
+            <div class="orderform-header-text">
+              <h1 id="orderform-heading">Your shopping basket</h1>
+            </div>
+            <div class="orderform-header-images">
+              <a href="https://www.blinds-2go.co.uk/">
+                <img src="/arquivos/b2g-logo.svg" />
+              </a>
+              <a  href="https://www.curtains-2go.co.uk/">
+                <img src="/arquivos/c2g-logo.svg" />
+              </a>
+            </div>
+          </div>
+        `
+
+        mainCart.prepend([reassuranceBar, orderFormHeader])
+
+        $('th.product').attr('id', 'cart-header-product')
+
     }
   }
 }
@@ -58,6 +82,6 @@ $(document).on('ready', function () {
     setTimeout(() => {
       shoppingCart.header.init()
       shoppingCart.mainCart.init()
-    }, 1000)
+    }, 500)
   }
 })
